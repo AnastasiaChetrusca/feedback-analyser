f29e46d563efc517d1ac5926890f05dac013edbb
AnastasiaChetrusca <achetrusca@yahoo.com>
2016-06-14 20:22:00 +0300

    first commit

--- amazon.py
+++ amazon.py
@@ -12,7 +12,7 @@ from dateutil import parser
 from pprint import pprint
 from lxml import etree
 
-nltk.download()
+
 
 page='http://www.amazon.com/Dekart-SIM-Card-Reader-Windows/product-reviews/B0045BIUGG/ref=cm_cr_getr_d_paging_btm_1?ie=UTF8&showViewpoints=1&sortBy=recent&pageNumber='
 
